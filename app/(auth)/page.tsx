9198f7bb8575db8fd33db70d63e17fc4ddda184f
Saehanseul <saehanseul@gmail.com>
2024-04-26 09:13:36 +0900

    init

---
+++ app/(auth)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>트위터</h1>
+      <h2>트위터에 오신걸 환영합니다!</h2>
+    </div>
+  );
+}
---
+++ app/swr-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { SWRConfig } from "swr";
+export const SWRProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => fetch(url).then((response) => response.json())
+      }}
+    >
+      {children}
+    </SWRConfig>
+  );
+};
